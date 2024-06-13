e585006936107587410c07856338a9a8140f6413
Gabriel <100089814+Estudianteclass@users.noreply.github.com>
2024-06-13 23:24:10 +0100

    Update Cliente.java
    cliente arreglado

--- src/Clases/Cliente.java
+++ src/Clases/Cliente.java
@@ -1,7 +1,7 @@
 package Clases;
 
-import java.time.Instant;
-import java.util.Date;
+import java.sql.Date;
+import java.util.Objects;
 
 public class Cliente implements Comparable<Cliente> {
 
@@ -9,21 +9,24 @@ public class Cliente implements Comparable<Cliente> {
     private String nombre, apellidos, nomEntrenador, direccion, clase;
     private Date fechaInscripcion;
 
-    public Cliente(String dni, String nombre, String apellidos, String nomEntrenador, String direccion, String clase, Date fechaInscripcion) {
+    public Cliente(String dni, String nombre, String apellidos, String direccion, Date fechaInscripcion, String nomEntrenador, String clase) {
         this.dni = dni;
         this.nombre = nombre;
         this.apellidos = apellidos;
-        this.nomEntrenador = nomEntrenador;
         this.direccion = direccion;
-        this.clase = clase;
         this.fechaInscripcion = fechaInscripcion;
+        this.nomEntrenador = nomEntrenador;
+        this.clase = clase;
+
     }
+
     public Cliente(String dni, String nomEntrenador, String direccion, String clase) {
         this.dni = dni;
         this.nomEntrenador = nomEntrenador;
         this.direccion = direccion;
         this.clase = clase;
     }
+
     public String getIdCliente() {
         return dni;
     }
@@ -99,5 +102,4 @@ public class Cliente implements Comparable<Cliente> {
     public String toString() {
         return "DNI:" + dni + ", nombre:" + nombre + ", apellidos: " + apellidos + ", nomEntrenador:" + nomEntrenador + ", direccion:" + direccion + ", clase: " + clase + ", fecha de Inscripcion:" + fechaInscripcion;
     }
-
 }
