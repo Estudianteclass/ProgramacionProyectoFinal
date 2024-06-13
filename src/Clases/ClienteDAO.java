072f697862cc1e5f8e0b98a58ac8f1d677b60e12
Gabriel <100089814+Estudianteclass@users.noreply.github.com>
2024-06-13 21:27:05 +0100

    Update ClienteDAO.java
    cambios en  metodos del dao

--- src/Clases/ClienteDAO.java
+++ src/Clases/ClienteDAO.java
@@ -2,10 +2,18 @@ package Clases;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ClienteDAO {
 
-    static final String URL = "jdbc:mysql://localhost:3306/empresa";
+    static final String URL = "jdbc:mysql://localhost:3306/gimnasio";
     static final String USER = "root";
     static final String CLAVE = "";
     private Connection conexion;
@@ -28,4 +36,114 @@ public class ClienteDAO {
 
     }
 
+    public Cliente read(String read) {
+        Cliente buscar = null;
+        if (read != null && !read.isBlank()) {
+            String sql = "SELECT*FROM clientes WHERE dni LIKE ?";
+            try (PreparedStatement sentencia = conexion.prepareStatement(sql)) {
+
+                sentencia.setString(1, read);
+                ResultSet rs = sentencia.executeQuery();
+                while (rs.next()) {
+                    String dni = rs.getString(1);
+                    String nom = rs.getString(2);
+                    String apellido = rs.getString(3);
+                    String direccion = rs.getString(4);
+                    Date alta = rs.getDate(5);
+                    String monitor = rs.getString(6);
+                    String clase = rs.getString(7);
+                    buscar = new Cliente(dni, nom, apellido, monitor, direccion, clase, alta);
+
+                }
+            } catch (SQLException ex) {
+                System.out.println("Codigo de error: " + ex.getErrorCode() + ", " + ex.getMessage());
+            }
+
+        }
+        return buscar;
+    }
+
+    public Cliente update(String id) {
+        Cliente cli = read(id);
+        if (cli != null) {
+            String sql = "UPDATE clientes " + "SET monitor=?,SET direccion=?,SET clase=?" + "WHERE dni = ?";
+
+            try (PreparedStatement sentencia = conexion.prepareStatement(sql)) {
+
+                sentencia.setString(1, cli.getNomEntrenador());
+                sentencia.setString(2, cli.getDireccion());
+                sentencia.setString(3, cli.getClase());
+                sentencia.setString(4, cli.getIdCliente());
+
+                sentencia.executeUpdate();
+
+            } catch (SQLException ex) {
+                System.out.println("Error al actualizar un cliente.");
+            }
+        }
+        return cli;
+    }
+
+    public boolean delete(String id) {
+        boolean borrar = false;
+
+        if (id != null && !id.isBlank()) {
+            String sql = "DELETE FROM clientes WHERE dni LIKE ?";
+
+            try (PreparedStatement sentencia = conexion.prepareStatement(sql)) {
+
+                sentencia.setString(1, id);
+                sentencia.executeUpdate();
+                borrar = true;
+            } catch (SQLException ex) {
+                System.out.println("Codigo de error: " + ex.getErrorCode() + ", " + ex.getMessage());
+            }
+        }
+        return borrar;
+    }
+
+    public boolean create(Cliente clien) {
+        boolean insertado = false;
+        String sql = "INSERT INTO  clientes values( ?,  ?,  ?,  ?, ?, ? ,?  )";
+        try {
+            PreparedStatement sentencia = conexion.prepareStatement(sql);
+            sentencia.setString(1, clien.getIdCliente());
+            sentencia.setString(2, clien.getNombre());
+            sentencia.setString(3, clien.getApellidos());
+            sentencia.setDate(4, clien.getFechaInscripcion());
+            sentencia.setString(5, clien.getDireccion());
+            sentencia.setString(6, clien.getNomEntrenador());
+            sentencia.setString(7, clien.getClase());
+            sentencia.executeUpdate();
+            insertado = true;
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        return insertado;
+    }
+
+    public ArrayList<Object[]> mostrarTodos() {
+
+        ArrayList<Object[]> listado = new ArrayList<>();
+        Cliente insertar = null;
+        String sql = "SELECT*FROM clientes";
+
+        try (Statement sentencia = conexion.createStatement()) {
+            ResultSet rs = sentencia.executeQuery(sql);
+
+            while (rs.next()) {
+
+                Object[] fila = new Object[7];
+                for (int i = 0; i <= 6; i++) {
+                    fila[i] = rs.getObject(i + 1);
+
+                }
+                listado.add(fila);
+
+            }
+        } catch (SQLException ex) {
+            System.out.println("Codigo de error: " + ex.getErrorCode() + ", " + ex.getMessage());
+        }
+        return listado;
+    }
 }
