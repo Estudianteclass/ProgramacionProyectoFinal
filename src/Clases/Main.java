cb838b070bd78eb6122ec87b0c0cd07ec9f3de30
Gabriel <100089814+Estudianteclass@users.noreply.github.com>
2024-06-13 21:28:01 +0100

    Update Main.java
    actualizado el main para que pueda ejecutar el panel

--- src/Clases/Main.java
+++ src/Clases/Main.java
@@ -5,7 +5,9 @@ package Clases;
 public class Main {
     
      public static void main(String[] args) {
-        
+        Panel panel = new Panel();
+        panel.setVisible(true);
+        panel.setLocationRelativeTo(null);
     }
     
 }
